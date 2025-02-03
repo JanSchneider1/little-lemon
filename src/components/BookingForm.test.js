6fc8b14350dca68ff04b0112159f56d22ae66a8c
Schneider, Jan <jan.schneider@adesso.de>
2025-02-03 10:26:39 +0100

    Added Unit-Tests.

--- src/components/BookingForm.test.js
+++ src/components/BookingForm.test.js
@@ -1,6 +1,7 @@
 import {render, screen} from "@testing-library/react";
 import '@testing-library/jest-dom'
 import BookingForm from './BookingForm';
+import {userEvent} from "@testing-library/user-event";
 
 test('renders the BookingForm', () => {
     render(
@@ -9,4 +10,31 @@ test('renders the BookingForm', () => {
     );
     const headingElement = screen.getByText("Choose date");
     expect(headingElement).toBeInTheDocument();
+})
+
+test('selects date', async () => {
+    render(
+        <BookingForm availableTimes={["19:00", "20:00", "21:00"]}
+                     updateAvailableTimes={() => {}}/>
+    );
+
+    const dateInput = screen.getByLabelText("Choose date");
+    dateInput.setAttribute("value", "2035-01-01");
+
+    expect(dateInput).toHaveValue("2035-01-01");
+})
+
+test('selects occasion', async () => {
+    render(
+        <BookingForm availableTimes={["19:00", "20:00", "21:00"]}
+                     updateAvailableTimes={() => {}}/>
+    );
+
+    const occasionSelect = screen.getByRole('combobox', { name: /occasion/i });
+
+    const selectValue = "Birthday";
+    await userEvent.selectOptions(occasionSelect, selectValue);
+
+    expect(occasionSelect).toHaveValue(selectValue);
+    expect(screen.getByRole('option', {name: selectValue}).selected).toBe(true);
 })
